4e596589cf5cbcd3851a08650ac1c899adfa7050
2024ashikul <2024ashikul@gmail.com>
2025-07-13 01:06:52 +0600

    asssds

--- api/index.py
+++ api/index.py
@@ -1,10 +1,4 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return jsonify(message="Hello from Flask on Vercel!")
+from app import app
 
 # For Vercel Python serverless function
 def handler(environ, start_response):
